6e4301decbb475ec12341a643b06a8aa70348006
doro041 <64807351+doro041@users.noreply.github.com>
2021-05-13 15:11:19 +0100

    Create firebase.js

---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBP0mXAjwfnI_MIVnZI7tLjHPlK27mYG3A",
+    authDomain: "powerjuice-f025a.firebaseapp.com",
+    projectId: "powerjuice-f025a",
+    storageBucket: "powerjuice-f025a.appspot.com",
+    messagingSenderId: "319333397651",
+    appId: "1:319333397651:web:d11e3ba7d7bab477dd0966",
+    measurementId: "G-Y0QZK7VS1Q"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase;
